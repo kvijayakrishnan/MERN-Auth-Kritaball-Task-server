1a56dd13b2b885549d34aa1ebd4f2e802d5b361d
kvijayakrishnan <vijayakrishnankbv@yahoo.in>
2024-09-10 07:32:47 +0530

    added cors

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const express = require('express')
 const db = require('./db/db')
 const dotenv = require('dotenv')
 const userRoute = require('./routes/userRoute')
-
+const cors = require('cors')
 
 
 dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use(cors({origin:'*'}))
 db()
-
 
 app.use('/api', userRoute)
 
